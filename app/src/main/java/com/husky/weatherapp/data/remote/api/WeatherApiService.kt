b813e8708c0084ccbac4453c5f60493bd0bc5014
Rabi-Midnight <michal.rabinski@gmail.com>
2025-05-25 12:30:03 +0200

    Fixed api query

--- app/src/main/java/com/husky/weatherapp/data/remote/api/WeatherApiService.kt
+++ app/src/main/java/com/husky/weatherapp/data/remote/api/WeatherApiService.kt
@@ -9,8 +9,8 @@ interface WeatherApiService {
     suspend fun getCurrentWeather(
         @Query("latitude") lat: Double,
         @Query("longitude") lon: Double,
-        @Query("current") current: String = "",
-        @Query("daily") daily: String = "",
+        @Query("current") current: String = "temperature_2m,relative_humidity_2m,wind_speed_10m,weather_code",
+        @Query("daily") daily: String = "temperature_2m_max,temperature_2m_min,weather_code",
         @Query("forecast_days") days: Int = 5,
         @Query("timezone") timezone: String = "auto"
     ): WeatherResponseDto
