bbb3713e563ed9b2877f07ec150d2f93dc389555
Rabi-Midnight <michal.rabinski@gmail.com>
2025-05-25 09:30:57 +0200

    Initiated injection placeholders

--- app/src/main/java/com/husky/weatherapp/MainApplication.kt
+++ app/src/main/java/com/husky/weatherapp/MainApplication.kt
@@ -1,6 +1,25 @@
 package com.husky.weatherapp
 
 import android.app.Application
+import com.husky.weatherapp.di.presentationModuleInjections
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+import org.koin.core.logger.Level
 
 class MainApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        startKoin {
+            androidLogger(Level.ERROR)
+            androidContext(this@MainApplication)
+            modules(
+                listOf(
+                    presentationModuleInjections
+                )
+            )
+        }
+    }
+
 }
---
+++ app/src/main/java/com/husky/weatherapp/di/PresentationModuleInjections.kt
@@ -0,0 +1,9 @@
+package com.husky.weatherapp.di
+
+import org.koin.dsl.module
+
+val presentationModuleInjections = module {
+
+
+
+}
